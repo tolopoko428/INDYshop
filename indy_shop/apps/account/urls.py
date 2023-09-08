f172ea6db2e8dca83a35703f4946784c945e7379
Kamil <premium.version.1.0@gmail.com>
2023-09-08 13:30:43 +0600

    account_prifile

--- indy_shop/apps/account/urls.py
+++ indy_shop/apps/account/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from apps.account import views
 
 
+
 urlpatterns = [
-    path('registration/', views.register_user, name = 'register'),
-    path('login/', views.LoginView.as_view(), name = 'login'),
-    path('index/', views.index, name='index'),
+    path('registration/', views.register_user, name='registration'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('dashboard/logout/', views.user_logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
+
 
